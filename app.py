311daf16e6d53a30154bb264fe114ada158ea6ef
dmorhain <dmo@mail.com>
2017-01-11 15:32:02 +0000

    commit 11.01.2017 16:30

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis = Redis(host='0.0.0.0', port=6379)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
