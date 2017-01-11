f53725a43ede4fcf957323b7b897c570e46b3ec6
dmorhain <dmo@mail.com>
2017-01-11 15:18:04 +0000

    commit 11.01.2017 16:15

--- app.py
+++ app.py
@@ -7,9 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Engie IT Cloud Bar / One Cloud Team
-
-Docker CI/CD demo - Thursday, January 12th 2017. I have been seen %s times.' % redis.get('hits')
+    return 'Docker Demo - Tuesday  demos should always work Happy Friday. I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
